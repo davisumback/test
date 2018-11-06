de8b730b289ce09a4dd12308cdfd184b102306c1
davisumback <davisumback@hotmail.com>
2018-11-06 12:11:22 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1,  maximum-scale=1, shrink-to-fit=no">
+        <meta name="keywords" content="">
+
+        <link rel="icon" type="favicon" sizes="96x96" href="img/favicon.ico">
+
+        <title>MedContabil | Contabilidade Médica</title>
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <!-- <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto:300" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top font-title-padrao nav-custom">
+            <a class="navbar-brand nav-item-custom" href="#topo-site">
+                <img class="nav-logo" src="img/logo_marca.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item nav-item-custom">
+                        <a class="nav-link" href="#topo-site">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item nav-item-custom">
+                        <a class="nav-link" href="#sobre">Sobre Nós</a>
+                    </li>
+                    <li class="nav-item nav-item-custom">
+                        <a class="nav-link" href="#funciona">Como Funciona</a>
+                    </li>
+                    <li class="nav-item nav-item-custom">
+                        <a class="nav-link" href="#precos">Preços</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="img-header font-title-padrao" id="topo-site">
+            <div class="container text-white animated fadeInLeft slow">
+                <div class="row pd-texto">
+                    <div class="col-md-6">
+                        <h2 class="texto-header-1 tirar-margin">Cresça com a</h2>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-md-6">
+                        <h1 class="texto-header-2"><b>medcontábil</b></h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-9">
+                        <h2 class="texto-header-1">Especialistas em</h2>
+                        <h2 class="texto-header-2"><b>Contabilidade Médica</b></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- **************************** SEÇÃO SOBRE ********************* -->
+
+        <section class="bg-section-1" id="sobre">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="row">
+                            <div class="col text-center font-title-padrao">
+                                <h1 class="title-sobre">Conheça mais sobre nós!</h1>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row font-txt-padrao">
+                            <div class="col-md-3 text-center mt-5" >
+                                <img src="img/sobre_1.png" alt="">
+                            </div>
+                            <div class="col-md-6 txt-1 mt-5">
+                                <h6 class="txt-sobre">
+                                    Tecnologia e profissionalismo aliados para um atendimento
+                                    personalizado, que visa proporcionar o máximo de qualidade,
+                                    excelência e satisfação.
+                                </h6>
+                            </div>
+                        </div>
+                        <div class="row mt-5 font-txt-padrao">
+                            <div class="col-md-4 text-center">
+                                <img class="icon-custom" src="img/sobre_2.png" alt="">
+                            </div>
+                            <div class="col-md-6 txt-1">
+                                <h6 class="txt-sobre">
+                                    Com foco no atendimento personalizado, nossa plataforma
+                                    digital permite que você realize os mais diversos serviços
+                                    voltados para contabilidade. Sendo assim, sua empresa se mantém
+                                    organizada e com o resultado que você merece.
+                                </h6>
+                            </div>
+                        </div>
+                        <div class="row mt-5 mb-5 font-txt-padrao">
+                            <div class="col-md-5 text-center">
+                                <img class="icon-custom" src="img/sobre_3.png" alt="">
+                            </div>
+                            <div class="col-md-6 txt-1">
+                                <h6 class="txt-sobre">
+                                    Contando com uma gama de recursos disponíveis, seu processo
+                                    de trabalho se torna mais ágil, otimizando suas tarefas, seja
+                                    pelo computador ou via mobile. Um show de recursos que
+                                    podem estar ao seu dispor na palma da sua mão.
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ********************* SEÇÃO MAPA ******************* -->
+
+        <section class="bg-section-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center font-title-padrao">
+                        <h2 class="titulo-mapa">ATENDEMOS TODO BRASIL</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 text-center">
+                        <img class="mt-5 tamanho-mapa" src="img/mapabr3.png" alt="">
+                    </div>
+                    <div class="col-md-6 font-txt-padrao text-center">
+                        <h3 class="mt-5 texto-mapa">Com uma equipe altamente especializada no segmento,
+                            amamos o que fazemos, realizando um trabalho e fornecendo suporte
+                            em todo o território nacional. Não fazemos diferente, fazemos a diferença.
+                            Conte conosco!
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ************************ SEÇÃO FUNCIONA ********************** -->
+
+        <section class="bg-section-2" id="funciona">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center font-title-padrao">
+                        <h1 class="titulo-funcao">COMO FUNCIONA</h1>
+                        <p>Conheça como agimos</p>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-12">
+                        <div class="card card-voce-faz">
+                            <div class="card-body">
+                                <h5 class="card-title font-title-padrao titulo-card-funcao"><i class="fas fa-user-alt mr-3"></i><b>VOCÊ FAZ</b></h5>
+                                <hr>
+                                <div class="card-text">
+                                    <ul class="font-title-padrao">
+                                        <li>
+                                            <h6 class="texto-card-funcao"><b>Emissão de notas fiscais</b></h6>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-6">
+                        <div class="card card-padrao">
+                            <div class="card-body">
+                                <h5 class="card-title font-title-padrao titulo-card-funcao"><i class="fas fa-building mr-3"></i><b>NÓS FAZEMOS</b></h5>
+                                <hr>
+                                <div class="card-text">
+                                    <ul class="ponto-lista font-title-padrao">
+                                        <li>
+                                            <h6><b>Gestão contábil empresarial</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Abertura da sua empresa</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Transferência gratuita de escritório</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Cálculo e emissão de guia de impostos</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Envio do Imposto de Renda da empresa</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Relatórios contábeis</b></h6>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card card-padrao">
+                            <div class="card-body">
+                                <h5 class="card-title font-title-padrao titulo-card-funcao"><i class="fas fa-cart-plus mr-3"></i><b>SERVIÇOS ADICIONAIS</b></h5>
+                                <hr>
+                                <div class="card-text">
+                                    <ul class="ponto-lista font-title-padrao">
+                                        <li>
+                                            <h6><b>Declaração Imposto de Renda Pessoa Física</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Registro de Funcionários</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Documentos para licitações e credenciamentos</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Alteração do contrato social</b></h6>
+                                        </li>
+                                        <li>
+                                            <h6><b>Recálculos de guias da empresa</b></h6>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col text-center">
+                        <button class="btn btn-lg button-custom font-title-padrao" type="submit" onclick="irWpp()"><b>Seja Cliente</b></button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ************************ SEÇÃO PREÇOS ************************ -->
+
+        <section class="bg-precos" id="precos">
+            <div class="container text-white">
+                <div class="row justify-content-center">
+                    <div class="col text-right font-title-padrao">
+                        <h1 class="titulo-precos"><b>CONFIRA NOSSO PREÇO</b></h1>
+                        <h4 class="texto-precos">Um dos melhores do mercado!</h4>
+                    </div>
+                </div>
+                <div class="row justify-content-center font-txt-padrao">
+                    <div class="col text-right mt-5">
+                        <h5 class="card-title"></h5>
+                        <h4 class="card-text sombra-texto">Contabilidade</h4>
+                        <p class="sombra-texto">a partir de:</p>
+                        <h1 class="font-title-padrao sombra-texto"><b>R$ 178,00</b></h1>
+                        <p class="sombra-texto">Converse com a gente</p>
+                        <button class="btn btn-lg button-custom" type="button" onclick="irWpp()"><b>Contato</b></button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ********************* SEÇÃO DE FINAL ******************* -->
+
+        <section class="bg-section-4 text-white">
+            <div class="container">
+                <div class="row justify-content-around">
+                    <div class="col-md-5 mb-5">
+                        <h4><b>Nossos contatos</b></h4>
+                        <hr>
+                        <h5 class="mb-4"><i class="fas fa-phone icon-contatos mr-3"></i>(41) 99998-0038</h5>
+                        <h5 class="mb-4"><i class="fas fa-envelope icon-contatos mr-3"></i>contato@medcontabil.com.br</h5>
+                        <div class="row">
+                            <div class="col-1">
+                                <i class="fas fa-map-marker-alt icon-contatos mr-3"></i>
+                            </div>
+                            <div class="col-11">
+                                <h5 class="txt-contatos">
+                                    Rua Francisco Rocha, 198 - CEP 80420-130  Batel, Curitiba - Paraná
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <!-- <h5 class="mb-">Fique por dentro!</h5>
+                        <h6 class="mb-3">Acompanhe nossas redes sociais</h6>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <button class="btn btn-lg button-media" type="button"><i class="fab fa-facebook-square icon-medias-contato"></i></button>
+                            </div>
+                            <div class="col-md-3">
+                                <button class="btn btn-lg button-media" type="button"><i class="fab fa-instagram icon-medias-contato"></i></button>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ********************* SEÇÃO DE FOOTER ******************* -->
+
+        <footer class="bg-section-4 text-white">
+            <div class="row">
+                <div class="col text-center">
+                    <p><b>MEDCONTABIL</b></p>
+                </div>
+            </div>
+        </footer>
+
+        <!-- WhatsHelp.io widget -->
+        <script type="text/javascript">
+            (function () {
+                var options = {
+                    whatsapp: "+554199998-0038", // WhatsApp number
+                    call_to_action: "Olá", // Call to action
+                    position: "left", // Position may be 'right' or 'left'
+                };
+                var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+                var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+                s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+                var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+            })();
+        </script>
+        <!-- /WhatsHelp.io widget -->
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+        <script type="text/javascript">
+            AOS.init();
+            function irWpp(){
+                window.open("https://api.whatsapp.com/send?phone=5541999980038&text=Olá%20Tudo%20bem?%20Poderia%20me%20atender?");
+            };
+        </script>
+    </body>
+</html>
